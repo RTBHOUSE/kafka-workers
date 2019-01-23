55a7375d26be54aa2a68ba65a1cede04dc5aa6fa
Bartosz Los <bartosz.los@rtbhouse.com>
2019-01-23 17:49:05 +0100

    Add consumer commit retries + minor consumer fixes

--- src/main/java/com/rtbhouse/kafka/workers/impl/consumer/OffsetCommitCallbackImpl.java
+++ src/main/java/com/rtbhouse/kafka/workers/impl/consumer/OffsetCommitCallbackImpl.java
@@ -1,9 +1,12 @@
 package com.rtbhouse.kafka.workers.impl.consumer;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.rtbhouse.kafka.workers.api.WorkersConfig;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.consumer.OffsetCommitCallback;
+import org.apache.kafka.clients.consumer.RetriableCommitFailedException;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +19,18 @@ public class OffsetCommitCallbackImpl implements OffsetCommitCallback {
 
     private static final Logger logger = LoggerFactory.getLogger(OffsetCommitCallbackImpl.class);
 
+    private final WorkersConfig config;
     private final ConsumerThread<?, ?> consumerThread;
     private final OffsetsState offsetsState;
     private final WorkersMetrics metrics;
+    private final AtomicInteger attempts = new AtomicInteger();
 
     public OffsetCommitCallbackImpl(
+            WorkersConfig config,
             ConsumerThread<?, ?> consumerThread,
             OffsetsState offsetsState,
             WorkersMetrics metrics) {
+        this.config = config;
         this.consumerThread = consumerThread;
         this.offsetsState = offsetsState;
         this.metrics = metrics;
@@ -32,10 +39,23 @@ public class OffsetCommitCallbackImpl implements OffsetCommitCallback {
     @Override
     public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
         if (exception != null) {
-            logger.error("commit failed, offsets: {} exception: {}", offsets, exception);
-            consumerThread.shutdown(new FailedCommitException(exception));
+            if (exception instanceof RetriableCommitFailedException) {
+                int attempt = attempts.incrementAndGet();
+                if (attempt <= config.getInt(WorkersConfig.CONSUMER_COMMIT_RETRIES)) {
+                    logger.warn("retriable commit failed exception: {}, offsets: {}, attempt: {}/{}",
+                            exception, offsets, attempt, config.getInt(WorkersConfig.CONSUMER_COMMIT_RETRIES));
+                } else {
+                    logger.error("retriable commit failed exception: {}, offsets: {}, attempt: {}/{}",
+                            exception, offsets, attempt, config.getInt(WorkersConfig.CONSUMER_COMMIT_RETRIES));
+                    consumerThread.shutdown(new FailedCommitException(exception));
+                }
+            } else {
+                logger.error("commit failed exception: {}, offsets: {}", exception, offsets);
+                consumerThread.shutdown(new FailedCommitException(exception));
+            }
         } else {
             logger.debug("commit succeeded, offsets: {}", offsets);
+            attempts.set(0);
             for (Map.Entry<TopicPartition, OffsetAndMetadata> entry : offsets.entrySet()) {
                 TopicPartition partition = entry.getKey();
                 long offset = entry.getValue().offset();
